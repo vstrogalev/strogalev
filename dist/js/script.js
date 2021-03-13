c6af8eaac02b5cd76e9ef3020d607849a81847b5
Viacheslav <strogalev@hotmail.com>
2021-03-13 11:30:55 +0300

    Mailer is done. Validation of form. Some adaptation. Validate is not working data sending to server

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,18 +1,75 @@
-const   hamburger   = document.querySelector('.hamburger'),
-        menu        = document.querySelector('.menu'),
-        menuClose   = document.querySelector('.menu__close');
+document.addEventListener('DOMContentLoaded', function () {
+	// hamburger -> menu open close
+	const hamburger = document.querySelector('.hamburger'),
+		menu = document.querySelector('.menu'),
+		menuClose = document.querySelector('.menu__close');
+		overlayClose = document.querySelector('.menu__overlay');
 
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
+	hamburger.addEventListener('click', () => {
+		menu.classList.add('active');
+	});
+
+	menuClose.addEventListener('click', () => {
+		menu.classList.remove('active');
+	});
+
+	overlayClose.addEventListener('click', () => {
+		menu.classList.remove('active');
+	});
+
+	// Fill percentage bars in the skills section
+	const levelItems = document.querySelectorAll('.skills__level-percent'),
+		levelProgressFront = document.querySelectorAll('.skills__level-progressFront');
+
+	levelItems.forEach((item, i) => {
+		levelProgressFront[i].style.width = item.innerHTML;
+	});
+
+	$('#contactForm').on('submit', function(e){
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function () {
+			$(this).find("input").val("");
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+	
+	validateForm('#contactForm');
+
+	// Form validation 
+	function validateForm(formSelector) {
+		$(formSelector).validate({
+			rules: {
+				userName: {
+					required: true,
+					minlength: 2
+				},
+				userMessage: {
+					required: true,
+					minlength: 5
+				},
+				userEmail: {
+					required: true,
+					email: true
+				},
+				policyAgree: "required"
+			},
+			messages: {
+				userName: {
+					required: "Пожалуйста, введите свое имя",
+					minlength: jQuery.validator.format("Введите {0} символа!")
+				},
+				userEmail: {
+					required: "Пожалуйста, введите свою почту",
+					email: "Неправильно введен адрес почты"
+				},
+				userMessage: "Пожалуйста, введите своё сообщение",
+				policyAgree: "Пожалуйста, согласитесь с политикой конфиденциальности"
+			}
+		});
+	};
 });
-
-menuClose.addEventListener('click', () => {
-    menu.classList.remove('active');
-});
-
-const levelItems = document.querySelectorAll('.skills__level-percent'),
-    levelProgressFront = document.querySelectorAll('.skills__level-progressFront');
-
-levelItems.forEach((item, i) => {
-    levelProgressFront[i].style.width = item.innerHTML;
-})
---
+++ dist/mailer/smart.php
@@ -0,0 +1,45 @@
+<?php 
+
+$name = $_POST['userName'];
+$email = $_POST['userEmail'];
+$message = $_POST['userMessage'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'mail.strogalev.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'webmaster@strogalev.com';                 // Наш логин
+$mail->Password = 'Jfm315102';                           // Наш пароль от ящика
+// $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->SMTPAutoTLS = false;
+$mail->SMTPSecure = false;
+$mail->Port = 25;				// $mail->Port = 465;    // TCP port to connect to
+ 
+$mail->setFrom('webmaster@strogalev.com', 'MyHosting');   // От кого письмо 
+$mail->addAddress('strogalev@hotmail.com');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные формы с сайта strogalev.com';
+$mail->Body    = 'Пользователь оставил данные <br>
+				<p>Имя: ' . $name . '</p> <br>
+				<p>E-mail: ' . $email . '</p>
+				<br><p>Сообщение: ' . $message . '</p>';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
